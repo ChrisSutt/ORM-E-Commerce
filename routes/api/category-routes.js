1e8cefda2e0c8919c667b9dd2fb26be9e4c7d100
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-07-31 12:05:15 -0300

    added find / create catagories in catagory-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,19 +3,68 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-});
+    try {
+      const categoriesWithProducts = await Category.findAll({
+        include: {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      });
+    
+      if (categoriesWithProducts.length === 0) {
+        return res.status(404).json({ message: 'No categories found' });
+      }
+    
+      res.json(categoriesWithProducts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
-router.get('/:id', (req, res) => {
+
+  router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+    try {
+      const categoriesWithProducts = await Category.findOne({
+        where: {
+          id: req.params.id
+        },
+        include: {
+          model: Product,
+          attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+        }
+      });
+  
+      if (!categoriesWithProducts) {
+        return res.status(404).json({ message: 'No categories found' });
+      }
+  
+      res.json(categoriesWithProducts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
-router.post('/', (req, res) => {
-  // create a new category
-});
+  
+  router.post('/', async (req, res) => {
+    // create a new category
+    try {
+      const categoriesWithProducts = await Category.create({
+        category_name: req.body.category_name
+      });
+  
+      res.json(categoriesWithProducts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json(err);
+    }
+  });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
